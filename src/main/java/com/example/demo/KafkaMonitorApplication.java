a8b389e3219f4bd9a37c6cfb00069cd4a6ab35bf
chaos-zheng <potatoazr171@163.com>
2026-01-09 16:03:27 +0800

    Add files via upload

---
+++ src/main/java/com/example/demo/KafkaMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMonitorApplication.class, args);
+	}
+
+}
