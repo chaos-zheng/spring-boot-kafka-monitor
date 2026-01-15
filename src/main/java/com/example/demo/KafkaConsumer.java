3d461f865f98516fbd190e38351737d6f45be82d
chaos-zheng <potatoazr171@163.com>
2026-01-15 14:41:41 +0000

    增加了睡眠时间，更好观察lag消费堆积

--- src/main/java/com/example/demo/KafkaConsumer.java
+++ src/main/java/com/example/demo/KafkaConsumer.java
@@ -14,16 +14,9 @@ public class KafkaConsumer {
     public void consume(String message, Acknowledgment ack) {
         System.out.println("收到消息: " + message);
         ack.acknowledge();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ignored) {
+        }
     }
-
-
-    // public void consume(String message) {
-    //     System.out.println("收到消息: " + message);
-
-    //     // 模拟消费慢一点，方便观察堆积
-    //     try {
-    //         Thread.sleep(1000);
-    //     } catch (InterruptedException ignored) {
-    //     }
-    // }
 }
